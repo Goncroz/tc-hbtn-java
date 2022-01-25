6f3982392dd2889fbc734133ae9a1440e51d5b42
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-01-25 19:58:55 -0300

    Formatando números

---
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -0,0 +1,19 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class PrintFormatacaoNumeros {
+
+	public static void main(String[] args) {
+		float taxa = 0.2456f;
+        float valor = 7654.321f;
+        Locale ptBr = new Locale("pt", "BR");
+        String formatado = NumberFormat.getCurrencyInstance(ptBr).format(valor);
+        	
+        /*NumberFormat nf = NumberFormat.getCurrencyInstance();
+        String formatado = nf.format(valor);*/
+        
+        System.out.printf("Valor: " + formatado + "2\n" + "Taxa: " + "%.2f", taxa);
+
+	}
+
+}
