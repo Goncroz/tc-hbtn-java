0ebb201fb296b5c14ff74ff36772f9e6be453f39
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-01-25 19:18:54 -0300

    Concatenação

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,7 @@
+public class PrintStringConcat {
+	public static void main(String[] args) {
+		String texto1 = "Holberton ", texto2 = "School", juntaString;
+		juntaString = texto1 + texto2;
+		System.out.println("Bem vindo a " + juntaString + "!\n");
+	}
+}
