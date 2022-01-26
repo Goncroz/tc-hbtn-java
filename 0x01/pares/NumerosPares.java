0796d2c5f06fbe47762f19bb54772df5330e560a
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-01-26 11:36:05 -0300

    Numeros pares de 0 a 100

--- 0x01/pares/NumerosPares.java
+++ 0x01/pares/NumerosPares.java
@@ -3,11 +3,11 @@ public class NumerosPares {
 
 	public static void main(String[] args) {
 		int num;
-		for(num = 0; num <= 100; num++) {
-		if(num%2 == 0 && num<100) {
+		for(num = 0; num <= 98; num++) {
+		if(num%2 == 0 && num<98) {
 			System.out.printf(num + ", ");	
 				}
-		else if(num%2 == 0 && num == 100){
+		else if(num%2 == 0 && num == 98){
 			System.out.println(num);
 		}
 		}
