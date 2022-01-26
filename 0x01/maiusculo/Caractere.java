509cc07c81171ada13677a6f0cad91b3b30722af
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-01-26 17:45:52 -0300

    Letra Maiúscula

--- 0x01/maiusculo/Caractere.java
+++ 0x01/maiusculo/Caractere.java
@@ -1,12 +1,11 @@
 public class Caractere {
 	
-	public static void main (String[] args) {
-	
-		System.out.println(Character.isUpperCase('a'));
-		System.out.println(Character.isUpperCase('B'));
-		System.out.println(Character.isUpperCase('d'));
-		System.out.println(Character.isUpperCase('f'));
-		System.out.println(Character.isUpperCase('G'));
-			 
-}
+	public static boolean EhMaiusculo(char c){
+		if (c >= 65 && c <= 90) {
+			return true;
+		}
+		else {
+		return false;
+		}
+	}
 }
