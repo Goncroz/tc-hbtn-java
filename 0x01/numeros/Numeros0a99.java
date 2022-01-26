1986b5190f8be79f36eee052a752c9e2981c91a5
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-01-26 11:12:35 -0300

    Numeros0a99.java

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,21 @@
+
+public class Numeros0a99 {
+
+	public static void main(String[] args) {
+			
+			int num;
+			for(num = 0; num < 100; num++)
+			if(num <= 98)
+				{
+				System.out.print(num + ", " );
+				
+			}else {
+				System.out.println(num);
+			}
+			
+		}
+		
+		
+
+	}
+
