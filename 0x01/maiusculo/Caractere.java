9f42cc4a8902a24fbaa4bd112b6d075f5a67bce6
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-01-26 17:52:52 -0300

    Letra Maiúscula

--- 0x01/maiusculo/Caractere.java
+++ 0x01/maiusculo/Caractere.java
@@ -1,11 +1,12 @@
 public class Caractere {
 	
 	public static boolean EhMaiusculo(char c){
-		if (c >= 65 && c <= 90) {
+		/*if (c >= 65 && c <= 90) {
 			return true;
 		}
 		else {
 		return false;
-		}
+		}*/
+		return(c >= 65 && c <=90);
 	}
 }
