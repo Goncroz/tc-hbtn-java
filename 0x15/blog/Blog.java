f09f598cded3d67650e11440d07635a9dbcccb4d
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-02-22 09:25:26 -0300

    Blog

---
+++ 0x15/blog/Blog.java
@@ -0,0 +1,55 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Blog {
+	
+	List<Post> posts;
+
+	public Blog() {
+		posts = new ArrayList<>();
+	}
+
+	public void adicionarPostagem(Post postagem) {
+		posts.add(postagem);
+		
+	}
+
+	public Set<String> obterTodosAutores() {
+		Set<String> autores = new TreeSet<>();
+		for (Post post : posts) {
+			autores.add(post.getAutor());
+		}
+		return autores;
+	}
+
+	public Map<String, Integer> obterContagemPorCategoria() {
+		Map<String, Integer> obterContagem = new TreeMap<>();
+		
+		for (Post post : posts) {
+			String categoria = post.getCategoria();
+			Integer contador = obterContagem.get(categoria);
+			
+			if(contador == null) {
+				obterContagem.put(categoria, 1);
+			}
+			else {
+				contador++;
+				obterContagem.replace(categoria, contador);
+			}
+		}
+		return obterContagem;
+	}
+	
+	
+	
+	
+	
+	
+
+}
