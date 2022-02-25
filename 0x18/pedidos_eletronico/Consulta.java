3b08c6f5d8aca740ff714adef73477941cb7dbad
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-02-25 15:03:33 -0300

    Relatório Pedidos com Eletrônicos

---
+++ 0x18/pedidos_eletronico/Consulta.java
@@ -0,0 +1,41 @@
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import consultarProdutos.ConsultaProdutos;
+
+public class Consulta {
+
+	public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+
+		List<Produto> listaProd = pedido.getProdutos().stream().filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
+				.collect(Collectors.toList());
+
+		return listaProd;
+
+	}
+	
+	public static Produto obterProdutoMaiorPreco(List<Produto> produtos){
+        
+		return produtos.stream()
+				.sorted(Comparator.comparing(Produto::getPreco).reversed()).findFirst().orElse(null);
+	}
+
+	public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, int menorPreco) {
+		
+		return produtos.stream().filter(t -> t.getPreco() >= menorPreco).collect(Collectors.toList());
+	}
+
+	public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedido) {
+		
+		List<Pedido> pedidosEletronicos = new ArrayList<>(pedido);
+		
+	 return pedidosEletronicos.stream()
+				.filter(t -> t.getProdutos().stream()
+						.anyMatch(p -> p.getCategoria() == CategoriaProduto.ELETRONICO)).collect(Collectors.toList());
+	}
+
+}
