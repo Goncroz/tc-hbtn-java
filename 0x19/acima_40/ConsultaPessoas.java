6cf143295acc3c64b54d2bfc47add156e567df85
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-03-02 13:26:40 -0300

    Pessoas agrupadas por cargo e que possuam pelo menos 40 anos

---
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -0,0 +1,40 @@
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+	public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> todasPessoas) {
+		
+		Map<String, List<Pessoa>> findByCargo = todasPessoas.stream().collect(Collectors.groupingBy(t -> t.getCargo()));
+		
+		
+		return findByCargo;
+	}
+
+	public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> todasPessoas) {
+		
+		Map<String, List<Integer>> findByIdade = todasPessoas.stream()
+				.collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+		
+		
+		return findByIdade;
+	}
+
+	public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> todasPessoas) {
+		
+		 Map<String, List<Pessoa>> findByQuarentaMais = todasPessoas.stream()
+				.collect(Collectors.groupingBy(
+						Pessoa::getCargo, Collectors.filtering(p -> p.equals(p), Collectors.toList())));
+		
+		 return findByQuarentaMais;
+	}
+	
+	
+	
+	
+
+}
