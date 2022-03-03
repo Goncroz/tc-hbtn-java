23c1c4f267f362f608b9ed25c497ef8baddf9d15
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-03-03 16:16:04 -0300

    Armazém de itens

---
+++ 0x22/armazem/Comida.java
@@ -0,0 +1,50 @@
+
+
+public class Comida {
+	
+	private String nome;
+	private double calorias;
+	private double preco;
+	
+	public Comida(String nome, double calorias, double preco) {
+		super();
+		this.nome = nome;
+		this.calorias = calorias;
+		this.preco = preco;
+	}
+
+	public Comida() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getCalorias() {
+		return calorias;
+	}
+
+	public void setCalorias(double calorias) {
+		this.calorias = calorias;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + nome + "] " + String.format("%f", calorias) + " R$ " + String.format("%f", preco);
+	}
+	
+	
+
+}
